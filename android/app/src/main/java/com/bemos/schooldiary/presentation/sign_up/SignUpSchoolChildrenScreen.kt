5f3e092a3cc2a00d1f4c03c023cfbc5919b6c67e
bemos <bemo.0@mail.ru>
2024-12-14 19:52:04 +0300

    - created a screen for registering schoolchildren

---
+++ android/app/src/main/java/com/bemos/schooldiary/presentation/sign_up/SignUpSchoolChildrenScreen.kt
@@ -0,0 +1,14 @@
+package com.bemos.schooldiary.presentation.sign_up
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignUpSchoolChildrenScreen(modifier: Modifier = Modifier) {
+
+    SignUpSchoolChildrenContent(
+        onRegisterClick = {
+
+        }
+    )
+}
