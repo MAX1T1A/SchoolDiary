dba53c9fab8eeb3e6d1267f757a6301b5783c832
bemos <bemo.0@mail.ru>
2024-12-15 01:00:45 +0300

    - make auth function in viewModel

--- android/app/src/main/java/com/bemos/schooldiary/presentation/sign_in/vm/SignInViewModel.kt
+++ android/app/src/main/java/com/bemos/schooldiary/presentation/sign_in/vm/SignInViewModel.kt
@@ -1,6 +1,31 @@
 package com.bemos.schooldiary.presentation.sign_in.vm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bemos.schooldiary.domain.models.AuthenticateUser
+import com.bemos.schooldiary.domain.use_case.AuthenticateUserUseCase
+import com.bemos.schooldiary.domain.use_case.TokenSetUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SignInViewModel : ViewModel() {
+class SignInViewModel @Inject constructor(
+    private val authenticateUserUseCase: AuthenticateUserUseCase,
+    private val tokenSetUseCase: TokenSetUseCase
+) : ViewModel() {
+
+    fun authUser(
+        authUser: AuthenticateUser
+    ) = viewModelScope.launch {
+        val response = authenticateUserUseCase.execute(
+            authUser
+        )
+        if (response.isSuccessful) {
+            val accessToken = response.body()?.access
+            tokenSetUseCase.execute(
+                authUser.email,
+                accessToken.toString()
+            )
+        }
+    }
+
 }
