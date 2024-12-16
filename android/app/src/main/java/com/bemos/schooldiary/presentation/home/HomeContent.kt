73dbc72eaf7a94d86c0fdcd4d328b853330fbbf3
bemos <bemo.0@mail.ru>
2024-12-16 21:20:27 +0300

    - make gigant verstca

--- android/app/src/main/java/com/bemos/schooldiary/presentation/home/HomeContent.kt
+++ android/app/src/main/java/com/bemos/schooldiary/presentation/home/HomeContent.kt
@@ -1,33 +1,84 @@
 package com.bemos.schooldiary.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bemos.schooldiary.R
+import com.bemos.schooldiary.presentation.home.utils.ui.calendar_view.EventCard
+import com.bemos.schooldiary.presentation.home.utils.ui.evets_item.EventItem
 
 @Composable
 fun HomeContent(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(top = 28.dp)
     ) {
-        Row {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
             Column {
                 Text(
-                    text = "Занятия начнутся"
+                    text = "Занятия начнутся",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp
                 )
                 Text(
                     text = "через  минут"
                 )
             }
+            Row(
+                modifier = Modifier
+                    .wrapContentWidth(Alignment.End),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.hope),
+                    contentDescription = null
+                )
+                Text(
+                    text = "124",
+                    fontSize = 23.sp,
+                    modifier = Modifier.padding(start = 6.dp)
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 28.dp)
+        ) {
+            LazyRow {
+                items(count = 20) {
+                    EventItem(text = "Олимпиада по химии")
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun HomeContentPreview() {
     HomeContent()
